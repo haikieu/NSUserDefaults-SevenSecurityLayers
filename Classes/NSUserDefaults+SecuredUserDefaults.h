c95dccf9db734cb5497249c8faa82dd4203aecaa
Hai Kieu <haikieu2907@gmail.com>
2014-07-22 23:39:47 +0700

    Create SecuredUserDefaults category

---
+++ Classes/NSUserDefaults+SecuredUserDefaults.h
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+SecuredUserDefaults.h
+//  DemoProject
+//
+//  Created by Hai Kieu on 7/22/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSUserDefaults (SecuredUserDefaults)
+
+@end
---
+++ Classes/NSUserDefaults+SecuredUserDefaults.m
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+SecuredUserDefaults.m
+//  DemoProject
+//
+//  Created by Hai Kieu on 7/22/14.
+//  Copyright (c) 2014 haikieu2907@gmail.com. All rights reserved.
+//
+
+#import "NSUserDefaults+SecuredUserDefaults.h"
+
+@implementation NSUserDefaults (SecuredUserDefaults)
+
+@end
